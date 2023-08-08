ab2f4aaf29d16fc1acd9cedf37bd4507bd2b521f
Pablo Mondino De La Rossa <p_mondino@hotmail.com>
2023-08-08 02:21:29 -0300

    trabajo terminado

--- main.js
+++ main.js
@@ -25,57 +25,71 @@ console.log(ingresar);
 if (ingresar) {
 
   let valorTotalTerreno = 1000;
+  let saldo = 0;
+  let cantidadDeCuotas = 1;
+  let entregaEnEfectivo = 0;
 
-  let elegirOpcion = prompt("Elegí tu opcion\n 1- Total valor pagando de contado\n 2-De cuanto es la entrega que desea realizar? \n 3-En cuantas cuotas desea financiar?\n 4-Cómo quedaría el plan de financiación elegido\n Presione x para salir");
+  let elegirOpcion = prompt("Elegí tu opcion\n 1- Valor total del terreno elegido\n 2-De cuanto es la entrega en efectivo que desea realizar? \n 3-¿Cual es el saldo para financiar?\n 4-¿En cuantas cuotas desea financiar el saldo?\n 5-Cómo quedaría el plan de financiación elegido\n Presione x para salir");
   while (elegirOpcion != "x") {
     switch (elegirOpcion) {
       case "1":
-        alert("elegiste 1\n        El valor total del terrenode contado es " + valorTotalTerreno)
+        alert("Elegiste la opción 1\n  El valor total del terreno es de " + valorTotalTerreno + " pesos");
 
         break;
 
       case "2":
-        let entregaEnEfectivo = parseFloat(prompt("De cuanto es la entrega en efectivo que deseas hacer?"));
+        entregaEnEfectivo = parseFloat(prompt("Elegiste la opcion 2\n ¿Cuanto es la entrega en efectivo que desea realizar?"));
+
         if (entregaEnEfectivo >= 200) {
-          alert("tu entrega está correcta. Supera el monto minimo. Te calculo de cuanto quedarán las 12 cuotas");
-          valorTotalTerreno = valorTotalTerreno - entregaEnEfectivo;
+          saldo = valorTotalTerreno - entregaEnEfectivo;
+          alert("Usted desea entregar " + entregaEnEfectivo + " pesos\n Tu entrega está correcta. Supera el monto minimo. ");
+
+          /*alert("entregando " + entregaEnEfectivo + "pesos el saldo queda en " + saldo + " pesos en 12 cuotas te quedaría en " + (saldo / 12));*/
 
         } else {
-          alert(" tu entrega dede ser igual o superio a 200 para poder financiar en cuotas")
+          alert(" tu entrega dede ser por lo menos igual o superio a 200 para poder financiar en cuotas")
         }
 
         break;
-        case "3":
-        let cantidadDeCuotas = parseFloat(prompt("elija una opcion: \n puede elegir de 12 a 36 cuotas"));
-            alert("elegiste " + cantidadDeCuotas +"cuotas, tienes que financiar   el saldo que es de " + saldo+ " pesos, en " + cantidadDeCuotas + " esto queda igual a " + (saldo * 2 /cantidadDeCuotas));
-            
-            /*if (entregaEnEfectivo >= 200) {
-              alert("tu entrega está correcta. Supera el monto minimo. Te calculo de cuanto quedarán las 12 cuotas");
-              valorTotalTerreno = valorTotalTerreno - entregaEnEfectivo;
-
-            } else {
-              alert(" tu entrega dede ser igual o superio a 200 para poder financiar en 12 cuotas")
-            }*/
-
-        break;  
 
       case "3":
-        alert("elegiste 3")
+        alert("Elegiste la opción 3\n  El saldo que desea financiar es de " + saldo + " pesos");
+
         break;
 
+      case "4":
+        cantidadDeCuotas = parseFloat(prompt("elija una opcion: \n puede elegir de 2 a 30 cuotas"));
+        if (cantidadDeCuotas <= 30 && cantidadDeCuotas >=2) {
+          alert("elegiste " + cantidadDeCuotas + "cuotas")
+        } else {
+          alert(" las cuotas deben ser entre  2 y 30 cuotas");
+        }
+
+
+        break;
+
+
+
+
+      case "5":
+        alert("Elegiste la opción 5 \n Valor total del lote es de " + valorTotalTerreno + " pesos. La entrega inicial es de " + entregaEnEfectivo + " y el saldo de " + saldo + " pesos a pagar en " + cantidadDeCuotas + " de cuotas de " + (saldo / cantidadDeCuotas) + " pesos");
+
+        break;
+
+
+
       default:
-        alert("Esta opcion no válida")
+        alert("Esta opcion no válida");
+
         break;
 
 
     }
-    elegirOpcion = prompt("Elegí tu opcion\n 1- Total valor pagando de contado\n 2-Calcular entrega y 12 cuotas \n 3-Calclar entrega y 24 cuotas\n Presione x para salir");
-
-    
+    elegirOpcion = prompt("Elegí tu opcion\n 1- Valor total del terreno elegido\n 2-De cuanto es la entrega en efectivo que desea realizar? \n 3-¿Cual es el saldo para financiar?\n 4-¿En cuantas cuotas desea financiar el saldo?\n 5-Cómo quedaría el plan de financiación elegido\n Presione x para salir");
   }
   alert("gracias por operar con nosotros");
- 
-  }
- else{
-    alert ("no podras seguir operando. LLama a nuestras oficina para solicitar un nuevo pin.")
+
+}
+else {
+  alert("no podras seguir operando. LLama a nuestras oficina para solicitar un nuevo pin.")
 }
